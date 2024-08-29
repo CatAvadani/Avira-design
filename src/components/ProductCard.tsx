7f52cf80f32bf0f53b401622319e05720df0a674
CatAvadani <catalinaavadani@gmail.com>
2024-08-29 18:54:32 +0200

    Add shopping cart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,5 @@
+import { HeartIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
+
 export interface ProductCardProps {
   id: number;
   title: string;
@@ -13,7 +15,13 @@ export default function ProductCard({ title, price, image }: ProductCardProps) {
       </div>
       <div className=' p-4 flex flex-col gap-4'>
         <h3 className='text-lg font-semibold mt-4'>{title}</h3>
-        <p className='text-lg font-bold text-indigo-600'>{price} KR</p>
+        <div className=' flex justify-between items-center'>
+          <p className='text-lg text-indigo-600'>{price} KR</p>
+          <div className=' flex justify-end items-center gap-4'>
+            <ShoppingBagIcon className='size-6 cursor-pointer' />
+            <HeartIcon className='size-7 cursor-pointer text-red-700' />
+          </div>
+        </div>
       </div>
     </div>
   );
